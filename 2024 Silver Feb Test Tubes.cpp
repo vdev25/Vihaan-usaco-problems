cb3ed697e10d73864fdfc23dbb8345d391653a91
vdev25 <86921938+vdev25@users.noreply.github.com>
2026-09-10 22:38:31 -0700

    added case where bottom liquids are equal

--- 2024 Silver Feb Test Tubes.cpp
+++ 2024 Silver Feb Test Tubes.cpp
@@ -20,8 +20,6 @@ int main() {
         t2.second += (b[k] != b[k + 1]);
     }
 
-    //cout << t1.first << " " << t1.second << " " << t2.first << " " << t2.second << endl;
-
     int minmoves;
 
     if (t1.first == 0 && t2.first == 0) {
@@ -35,15 +33,63 @@ int main() {
 
     if (t == 2 || t == 3) {
         if (t1.first == t2.first) {
-            if (t1.second % 2 != t2.second % 2) {
-                
-            } else {
-                
+            if (t1.second % 2 == t2.second % 2) {
+                if (t1.second >= t2.second) {
+                    cout << "1 2" << endl;
+                    t1.second -= 1;
+                }
+                else {
+                    cout << "2 1" << endl;
+                    t2.second -= 1;
+                }
             }
+            if (t1.second % 2 != t2.second % 2) {
+                if (t1.second % 2 == 0) {
+                    for (int k = 0; k <= t1.second; k = k + 1) {
+                        if (k % 2 == 0) {
+                            cout << "1 3" << endl;
+                        }
+                        else {
+                            cout << "1 2" << endl;
+                        }
+                    }
+                    for (int k = 0; k < t2.second; k = k + 1) {
+                        if (k % 2 == 0) {
+                            cout << "2 1" << endl;
+                        }
+                        else {
+                            cout << "2 3" << endl;
+                        }
+                    }
+                    cout << "3 2";
+                }
+                else {
+                    for (int k = 0; k <= t2.second; k = k + 1) {
+                        if (k % 2 == 0) {
+                            cout << "2 3" << endl;
+                        }
+                        else {
+                            cout << "2 1" << endl;
+                        }
+                    }
+                    for (int k = 0; k < t1.second; k = k + 1) {
+                        if (k % 2 == 0) {
+                            cout << "1 2" << endl;
+                        }
+                        else {
+                            cout << "1 3" << endl;
+                        }
+                    }
+                    cout << "3 1";
+                }
+            }
+        }
+        else {
+
         }
     }
 
-    
+
 
     return 0;
 }
